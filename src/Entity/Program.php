2120844c95d403970067ebf493236cbf82fcefb8
MehdiDAAJI <mehdidaaji@gmail.com>
2022-11-17 10:34:48 +0100

    quests 6

---
+++ src/Entity/Program.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProgramRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProgramRepository::class)]
+class Program
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $tilte = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $synospis = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $poster = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTilte(): ?string
+    {
+        return $this->tilte;
+    }
+
+    public function setTilte(string $tilte): self
+    {
+        $this->tilte = $tilte;
+
+        return $this;
+    }
+
+    public function getSynospis(): ?string
+    {
+        return $this->synospis;
+    }
+
+    public function setSynospis(string $synospis): self
+    {
+        $this->synospis = $synospis;
+
+        return $this;
+    }
+
+    public function getPoster(): ?string
+    {
+        return $this->poster;
+    }
+
+    public function setPoster(?string $poster): self
+    {
+        $this->poster = $poster;
+
+        return $this;
+    }
+}
